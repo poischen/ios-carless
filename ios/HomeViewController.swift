156e121c5cbd6d28d3017f97f879a3dd91ee3f11
Hila Safi <Hila@dhcp-10-176-87-182.dynamic.eduroam.mwn.de>
2017-11-04 13:29:13 +0100

    new project

---
+++ ios/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  ios
+//
+//  Created by Hila Safi on 01.11.17.
+//  Copyright © 2017 Hila Safi. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
