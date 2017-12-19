5373f84b26af1e426e906a4d95e372fb28ffe916
“ayech“ <ayech@cip.ifi.lmu.de>
2017-12-19 19:28:03 +0100

    readded Advertise View

---
+++ ios/View/Advertise/AdvertisePageContentVC.swift
@@ -0,0 +1,9 @@
+//
+//  AdvertisePageContentVC.swift
+//  ios
+//
+//  Created by admin on 19.12.17.
+//  Copyright © 2017 ioscars. All rights reserved.
+//
+
+import Foundation
---
+++ ios/View/Advertise/AvailibilityCalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  AvailibilityCalendarCell.swift
+//  ios
+//
+//  Created by admin on 19.12.17.
+//  Copyright © 2017 ioscars. All rights reserved.
+//
+
+import UIKit
+
+class AvailibilityCalendarCell: JTAppleCell {
+    
+}
