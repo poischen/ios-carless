ad8575c52ab26f7e2240f1a1a765af66a8d3a775
“ayech“ <ayech@cip.ifi.lmu.de>
2018-01-15 13:10:38 +0100

    adding profile owners offers collection view

--- ios/Controller/Profile/ExternProfileViewController.swift
+++ ios/Controller/Profile/ExternProfileViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 import Firebase
 import FirebaseDatabase
-
+import Cosmos
 
 class ExternProfileViewController: UIViewController {
     let storageAPI = StorageAPI.shared
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var profileOwnerImageView: UIImageView!
+    @IBOutlet weak var profileOwnerName: UILabel!
+    @IBOutlet weak var noCurrentOfferNote: UILabel!
+    @IBOutlet weak var profileOwnerRatingStars: CosmosView!
+    @IBOutlet weak var otherUsersAboutProfileOwnerHL: UILabel!
+    @IBOutlet weak var profileOwnersCurrentOffersHL: UILabel!
+    @IBOutlet weak var collectionViewRatings: UICollectionView!
+    @IBOutlet weak var collectionViewOffers: UICollectionView!
+
+    let collectionViewRatingsIdentifier = "RatingsCollectionViewCell"
+    let collectionViewOffersIdentifier = "OffersCollectionViewCell"
+    
+    let ratinsHL = "Other users about "
+    let offersHL = " current offers"
     
     var profileOwner: User?
     
@@ -26,17 +39,18 @@ class ExternProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imageView.layer.cornerRadius = imageView.frame.size.width / 2
-        imageView.clipsToBounds = true
-        imageView.layer.borderWidth = 2
-        imageView.layer.borderColor = UIColor.black.cgColor
+        profileOwnerImageView.layer.cornerRadius = profileOwnerImageView.frame.size.width / 2
+        profileOwnerImageView.clipsToBounds = true
+        profileOwnerImageView.layer.borderWidth = 2
+        profileOwnerImageView.layer.borderColor = UIColor.black.cgColor
         
-        let imageUrl = storageAPI.getUserProfileImageUrl(uID: storageAPI.userID()) { (path) in
-            let profileImgUrl = URL(string: path)
-            self.imageView.kf.indicatorType = .activity
-            self.imageView.kf.setImage(with: profileImgUrl)
+        if let user = profileOwner {
+            storageAPI.getUserProfileImageUrl(uID: user.id) { (path) in
+                let profileImgUrl = URL(string: path)
+                self.profileOwnerImageView.kf.indicatorType = .activity
+                self.profileOwnerImageView.kf.setImage(with: profileImgUrl)
+            }
         }
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -48,16 +62,27 @@ class ExternProfileViewController: UIViewController {
 extension ExternProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return ratingNames.count
+        if collectionView == self.collectionViewRatings {
+            return ratingNames.count
+        } else {
+            return 0 //todo
+        }
     }
     
+    //TODO: implement collectionView:willDisplayCell:forItemAtIndexPath: ???
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RatingsCollectionViewCell", for: indexPath) as! RatingsCollectionViewCell
-        
-        cell.userName.text = self.ratingNames[indexPath.row] + ":"
-        cell.userRatingDescription.text = self.ratingDescriptions[indexPath.row]
-        cell.userRatingStars.rating = self.ratingStars[indexPath.row]
-        
-        return cell
+        if collectionView == self.collectionViewRatings {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.collectionViewRatingsIdentifier, for: indexPath) as! RatingsCollectionViewCell
+            
+            cell.userName.text = self.ratingNames[indexPath.row] + ":"
+            cell.userRatingDescription.text = self.ratingDescriptions[indexPath.row]
+            cell.userRatingStars.rating = self.ratingStars[indexPath.row]
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.collectionViewOffersIdentifier, for: indexPath) as! ProfileUsersOffersCollectionViewCell
+            //todo: fill cell
+            return cell
+        }
     }
 }
