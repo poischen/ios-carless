a5a8c43d73dc36b3fcc7610ce629660b6c0ac0a8
“ayech“ <ayech@cip.ifi.lmu.de>
2018-01-25 18:58:07 +0100

    navigation

--- ios/Controller/Offer/OfferingViewController.swift
+++ ios/Controller/Offer/OfferingViewController.swift
@@ -52,10 +52,24 @@ class OfferingViewController: UIViewController {
     @IBAction func chatButton(_ sender: UIButton) {
         if let lessorUser = lessor {
             let storyboard = UIStoryboard(name: "ChatStoryboard", bundle: nil)
-            let vc = storyboard.instantiateViewController(withIdentifier: "ChatWithUser") as! ChatWindowVC
+            let nc = storyboard.instantiateViewController(withIdentifier: "NavControllerChatWindow") as! UINavigationController
+            let vc = nc.topViewController as! ChatWindowVC
+            
             vc.selectedUser = lessorUser.id
+            vc.cameFromOffer = true
+
+            let profileImageView = UIImageView()
+            profileImageView.image = UIImage(named: "ProfilePic")
+            let profileLessorImgUrl = URL(string: (lessorUser.profileImgUrl))
+            profileImageView.kf.setImage(with: profileLessorImgUrl)
+            vc.receiverImage = profileImageView.image
+            
             self.present(vc, animated: true, completion: nil)
         }
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func actionItem(_ sender: Any) {
