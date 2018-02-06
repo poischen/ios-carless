40d9586d91c091cc4591d2d0da6dd01aa5a40f11
“ayech“ <ayech@cip.ifi.lmu.de>
2018-02-06 01:29:50 +0100

    grey features icons, feature icon sort bug fix

--- ios/Model/Advertise.swift
+++ ios/Model/Advertise.swift
@@ -58,7 +58,6 @@ final class Advertise {
     //further values vor input UI
     let seatsPickerContent = ["1", "2", "3", "4", "5", "6", "7", "8", "more"]
     let seatsPickerIcons = [UIImage(named:"1"), UIImage(named:"2"), UIImage(named:"3"), UIImage(named:"4"), UIImage(named:"5"), UIImage(named:"6"), UIImage(named:"7"), UIImage(named:"8"), UIImage(named:"9")]
-    /*let timeContent = ["00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30", "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00", "23:30"]*/
     
     
     init(){
@@ -137,13 +136,14 @@ final class Advertise {
         for feature in features {
             let featureName = feature.name
             featuresDict.updateValue(feature.id, forKey: featureName)
-            featuresLabels.append(featureName)
             
             if let icon = UIImage(named: featureName) {
                 featuresImages.append(icon)
+                featuresLabels.append(featureName)
             } else {
                 let url = URL(string: feature.iconURL)!
                 KingfisherManager.shared.retrieveImage(with: url, options: nil, progressBlock: nil, completionHandler: { image, error, cacheType, imageURL in
+                    self.featuresLabels.append(featureName)
                     self.featuresImages.append(image!)
                 })
             }
